5a913655f613f4e28258ed5bd9e42c9d6fb153fb
Jeremias Santos Lima <jeremias.lima@agv.com.br>
2021-01-10 13:01:05 -0300

    finalizado

--- App.js
+++ App.js
@@ -3,18 +3,18 @@ import React, {useState} from 'react';
 
 import styled from 'styled-components';
 import DropDownPicker from 'react-native-dropdown-picker';
-import {Modal, Switch} from 'react-native';
+import {Modal, StatusBar, Switch} from 'react-native';
 import Slider from '@react-native-community/slider';
 
 //
 
 export default () => {
-  const [isName, setname] = useState('');
-  const [value, setValue] = useState();
-  const [valueSlider, setValueSlider] = useState();
-  const [isEnabled, setIsEnabled] = useState(false);
+  const [isName, setName] = useState(''); //Nome
+  const [sexo, setSexo] = useState();
+  const [valueSlider, setValueSlider] = useState(0); //Renda
+  const [isEnabled, setIsEnabled] = useState(false); //Estudante
   const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); //switch
 
   const abrirModal = () => {
     setIsOpen(true);
@@ -25,27 +25,32 @@ export default () => {
 
   return (
     <Pagina>
+      <StatusBar hidden={true} />
+      <ImagemLogo source={require('./src/assets/bank.png')} />
       <CampoTexto
-        value={isName}
         placeholder={'Seu nome'}
         placeholderTextColor={'#14213d'}
+        onChangeText={(value) => setName(value)}
       />
       <DropDownPicker
         items={[
-          {label: 'Masculino', value: 1},
-          {label: 'Feminino', value: 2},
+          {label: 'Masculino', value: 'Masculino'},
+          {label: 'Feminino', value: 'Feminino'},
         ]}
         placeholder={'Selecione o sexo'}
-        placeholderStyle={{color: '#14213d', fontSize: 20}}
-        defaultValue={value}
+        placeholderStyle={{color: '#14213d'}}
+        defaultValue={sexo}
         arrowSize={0}
+        labelStyle={{fontSize: 20}}
+        selectedLabelStyle={{color: 'black', fontSize: 20}}
         containerStyle={{height: 50, width: '90%'}}
-        style={{backgroundColor: '#fafafa'}}
+        style={{backgroundColor: '#fafafa', fontSize: 25}}
         itemStyle={{
           justifyContent: 'flex-start',
+          color: 'black',
         }}
         dropDownStyle={{backgroundColor: '#fafafa'}}
-        onChangeItem={(item) => setValue(item.value)}
+        onChangeItem={(item) => setSexo(item.value)}
       />
       <Subpagina>
         <TextoSlider>Informe a Renda mensal: Até R${valueSlider}</TextoSlider>
@@ -72,7 +77,7 @@ export default () => {
           value={isEnabled}
         />
       </Pagina2>
-      <Entrar onPress={abrirModal, setName()}>
+      <Entrar onPress={abrirModal}>
         <TextoEntrar>Entrar</TextoEntrar>
       </Entrar>
       <PaginaModal>
@@ -81,13 +86,43 @@ export default () => {
           animationType="slide"
           transparent={true}
           visible={isOpen}>
-          <Pagina3>
-            <Texto>Vamos Confirmar?</Texto>
-            <Texto>Seu nome é {isName}</Texto>
-            <Entrar onPress={fecharModal}>
-              <TextoEntrar>Entrar</TextoEntrar>
-            </Entrar>
-          </Pagina3>
+          {!isName || !sexo ? (
+            <ModalErro>
+              <TextoModalErro>Nome e Gênero são necessários!</TextoModalErro>
+              <ImagemErrou source={require('./src/assets/errou.png')} />
+              <Entrar onPress={fecharModal}>
+                <TextoEntrar>Voltar</TextoEntrar>
+              </Entrar>
+            </ModalErro>
+          ) : (
+            <Pagina3>
+              <TextoModalOk>Vamos Confirmar?</TextoModalOk>
+              <Pagina4>
+                <Texto>
+                  Nome:<TextoNegrito> {isName}</TextoNegrito>
+                </Texto>
+                <Texto>
+                  Gênero:<TextoNegrito> {sexo}</TextoNegrito>
+                </Texto>
+                <Texto>
+                  Renda Atual: <TextoNegrito>R${valueSlider},00</TextoNegrito>
+                </Texto>
+                {!isEnabled ? (
+                  <TextoItalico>Não é Estudante</TextoItalico>
+                ) : (
+                  <TextoItalico>É Estudante</TextoItalico>
+                )}
+              </Pagina4>
+
+              <Entrar onPress={fecharModal}>
+                <TextoEntrar>Entrar</TextoEntrar>
+              </Entrar>
+
+              <Corrigir onPress={fecharModal}>
+                <TextoCorrigir>Corrigir</TextoCorrigir>
+              </Corrigir>
+            </Pagina3>
+          )}
         </Modal>
       </PaginaModal>
     </Pagina>
@@ -141,6 +176,17 @@ const Entrar = styled.TouchableOpacity`
   background-color: #fca311;
   padding: 12px;
   border-radius: 10px;
+  margin: 5px 5px;
+`;
+const Corrigir = styled.TouchableOpacity`
+  width: 90%;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  background-color: #fca311;
+  padding: 12px;
+  border-radius: 10px;
+  margin: 5px 5px;
 `;
 const TextoEntrar = styled.Text`
   color: white;
@@ -158,5 +204,54 @@ const Pagina3 = styled.View`
   justify-content: center;
   align-items: center;
   border-radius: 15px;
-  background-color: #e5e5e5;
+  background-color: #aaa;
+`;
+const ModalErro = styled.View`
+  flex: 1;
+  margin: 20px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+  background-color: #14213d;
+`;
+const ImagemLogo = styled.Image`
+  width: 250px;
+  height: 250px;
+`;
+const ImagemErrou = styled.Image`
+  width: 300px;
+  height: 300px;
+`;
+const Pagina4 = styled.View`
+  border-radius: 15px;
+  background-color: #14213d;
+  width: 90%;
+  padding-left: 15px;
+  padding: 10px 10px;
+  margin: 10px 10px;
+`;
+const TextoModalOk = styled.Text`
+  color: #f0f0f0;
+  font-size: 25px;
+  font-weight: bold;
+`;
+const TextoModalErro = styled.Text`
+  color: #f0f0f0;
+  font-size: 25px;
+  font-weight: bold;
+`;
+const TextoNegrito = styled.Text`
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+`;
+const TextoItalico = styled.Text`
+  color: white;
+  font-size: 20px;
+  font-style: italic;
+`;
+const TextoCorrigir = styled.Text`
+  color: white;
+  font-size: 25px;
+  font-weight: bold;
 `;
